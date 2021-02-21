20faafbf9c38583d9b8a2821b571156ba03b04d0
Andrey Zaycev <dondrakone89@gmail.com>
2021-02-21 22:57:06 +0300

    Firstcommit

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _reactionRadius;
+    [SerializeField] private ParticleSystem particleSystem_piss;
+    private NavMeshAgent agent;
+
+
+    private void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        particleSystem_piss.Stop();
+    }
+    
+    private void Update()
+    {
+        if(Vector3.Distance(transform.position, _player.transform.position) <= _reactionRadius)
+        {
+            RaycastHit hit;
+            
+            if (Physics.Raycast(transform.position, _player.transform.position - transform.position, out hit, 200f))
+            {
+                if(hit.transform.tag == "Player")
+                {
+                    particleSystem_piss.Play();
+                    agent.SetDestination(_player.transform.position);
+                }
+                else
+                {
+                    particleSystem_piss.Stop();
+                }
+                     
+
+            }
+            else
+            {
+                particleSystem_piss.Stop();
+            }
+
+        
+        }
+        else
+        {
+            particleSystem_piss.Stop();
+        }
+         
+    }
+
+}
---
+++ Assets/Scripts/MoveControll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControll : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float _speed;
+    [SerializeField] private GameObject _flashLight;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            _flashLight.SetActive(!_flashLight.activeSelf);
+        }
+    }
+
+    //private void Update()
+    //{
+    //    _player.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+    //    _player.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical"));
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    _playerBody.AddForce(_player.transform.right * _speed * Input.GetAxis("Horizontal"), ForceMode.Force);
+    //    _playerBody.AddForce(_player.transform.forward * _speed * Input.GetAxis("Vertical"), ForceMode.Force);
+    //}
+}
