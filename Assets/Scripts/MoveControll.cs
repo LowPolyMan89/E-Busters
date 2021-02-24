2aa324303b320b72bca49a27152ef18be37725b0
LowPolyMan89 <dondrakone89@gmail.com>
2021-02-24 22:44:12 +0300

    new controll system

--- Assets/Scripts/MoveControll.cs
+++ Assets/Scripts/MoveControll.cs
@@ -7,11 +7,16 @@ public class MoveControll : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private float _speed;
     [SerializeField] private GameObject _flashLight;
+    [SerializeField] private Rigidbody _playerBody;
+    [SerializeField] private float depth;
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private Vector3 _cameraOffset;
 
 
     private void Start()
     {
-        
+        _playerBody = _player.GetComponent<Rigidbody>();
+        _cameraTransform = Camera.main.transform;
     }
 
     private void Update()
@@ -20,17 +25,21 @@ public class MoveControll : MonoBehaviour
         {
             _flashLight.SetActive(!_flashLight.activeSelf);
         }
+
+        Vector3 MouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, depth));
+        _player.transform.LookAt(MouseWorldPosition);
+        _player.transform.rotation = Quaternion.Euler(new Vector3(0, _player.transform.rotation.eulerAngles.y, 0));
+
+        _cameraTransform.position = new Vector3(_player.transform.position.x + _cameraOffset.x, _cameraOffset.y, _player.transform.position.z + _cameraOffset.z);
+
     }
 
-    //private void Update()
-    //{
-    //    _player.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-    //    _player.transform.position += new Vector3(0, 0, Input.GetAxis("Vertical"));
-    //}
 
-    //private void FixedUpdate()
-    //{
-    //    _playerBody.AddForce(_player.transform.right * _speed * Input.GetAxis("Horizontal"), ForceMode.Force);
-    //    _playerBody.AddForce(_player.transform.forward * _speed * Input.GetAxis("Vertical"), ForceMode.Force);
-    //}
+
+
+    private void FixedUpdate()
+    {
+        _playerBody.AddForce(Vector3.right * _speed * Input.GetAxis("Horizontal"), ForceMode.Force);
+        _playerBody.AddForce(Vector3.forward * _speed * Input.GetAxis("Vertical"), ForceMode.Force);
+    }
 }
